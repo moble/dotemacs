4e3d3081355bcee30fb6f310f165908b61324997
Michael Boyle <michael.oliver.boyle@gmail.com>
2014-11-26 00:11:59 -0500

    Make syntax consistent; closes #1

--- keybindings.el
+++ keybindings.el
@@ -1,20 +1,25 @@
 ;; These two are usually bound to minimization, which I hate passionately
-(global-unset-key "\C-z")
-(global-unset-key "\C-x\C-z")
+(global-unset-key (kbd "C-z"))
+(global-unset-key (kbd "C-x C-z"))
 (put 'suspend-frame 'disabled t)
 
+(global-set-key (kbd "M-e") 'execute-extended-command)
+(global-set-key (kbd "M-l") 'goto-line)
+(global-set-key (kbd "C-c C-c") 'compile)
+
 ;; Make some keys more Mac-like
-(global-set-key "\M-`" 'other-frame)
-(global-set-key [(meta a)] 'mark-whole-buffer)
-(global-set-key "\M-f" 'isearch-forward)
+(global-set-key (kbd "M-`") 'other-frame)
+(global-set-key (kbd "M-a") 'mark-whole-buffer)
+(global-set-key (kbd "M-f") 'isearch-forward-regexp)
+(global-set-key (kbd "M-s") 'save-buffer)
+(global-set-key (kbd "M-o") 'find-file)
 ;; Cut/copy/paste
-(global-set-key "\M-c" 'kill-ring-save)
-(global-set-key "\M-v" 'yank)
-(global-set-key "\M-x" 'kill-region)
+(global-set-key (kbd "M-c") 'kill-ring-save)
+(global-set-key (kbd "M-v") 'yank)
+;;(global-set-key (kbd "M-x") 'kill-region)
 ;; Undo/redo properly
-(global-set-key [(meta z)] 'undo)
-(global-set-key [C-z] 'undo)
-(global-set-key [(meta shift z)] 'redo)
+(global-set-key (kbd "M-z") 'undo)
+(global-set-key (kbd "M-S-z") 'redo)
 ;; "Zoom" the text
 (define-key global-map (kbd "M-=") 'text-scale-increase)
 (define-key global-map (kbd "M--") 'text-scale-decrease)
@@ -26,10 +31,7 @@
 (global-set-key (kbd "M-S-<left>") 'previous-interesting-buffer)
 
 ;; These correspond to some of my custom functions
-(global-set-key "\M-1" 'set-frame-position-one)
-(global-set-key "\M-t" 'toggle-frame-size)
-(global-set-key [(meta n)] 'night)
-(global-set-key [(meta d)] 'day)
-
-(global-set-key [(meta l)] 'goto-line)
-(global-set-key "\C-c\C-c" 'compile)
+(global-set-key (kbd "M-1") 'set-frame-position-one)
+(global-set-key (kbd "M-t") 'toggle-frame-size)
+(global-set-key (kbd "M-n") 'night)
+(global-set-key (kbd "M-d") 'day)
