9bd0be25fecb21de357299cdaaf8698e670e1f18
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-08-13 12:36:24 -0400

    Deactivate conda by default, since I'm not using python so much any more

--- packages.el
+++ packages.el
@@ -1,29 +1,29 @@
 ;; This must come before configurations of installed packages
 (when (>= emacs-major-version 24)
   (require 'package)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   (package-initialize)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 )
 
-;; ;; Use my anaconda installation for python
-;; (defvar python-shell-interpreter)
-;; (setq python-shell-interpreter
-;;       (expand-file-name "~/.continuum/current/envs/science/bin/python"))
+;; Use my anaconda installation for python
+(defvar python-shell-interpreter)
+(setq python-shell-interpreter
+      (expand-file-name "~/.continuum/current/envs/science/bin/python"))
 
-;; Tell everyone about my conda installation
-(require 'conda)
-(conda-env-initialize-interactive-shells)
-(conda-env-initialize-eshell)
-(conda-env-autoactivate-mode t)
-(setq conda-anaconda-home (file-truename "/Users/boyle/.continuum/current"))
-(setq conda-env-home-directory (file-truename "/Users/boyle/.continuum/current"))
-(conda-env-activate "science")
+;; ;; Tell everyone about my conda installation
+;; (require 'conda)
+;; (conda-env-initialize-interactive-shells)
+;; (conda-env-initialize-eshell)
+;; (conda-env-autoactivate-mode nil)
+;; (setq conda-anaconda-home (file-truename "/Users/boyle/.continuum/current"))
+;; (setq conda-env-home-directory (file-truename "/Users/boyle/.continuum/current"))
+;; (conda-env-activate "science")
 
-;; Anaconda-mode is apparently integrated with anaconda itself
-(add-hook 'python-mode-hook 'anaconda-mode)
+;; ;; Anaconda-mode is apparently integrated with anaconda itself
+;; (add-hook 'python-mode-hook 'anaconda-mode)
 
-;; Document function parameters in echo area
-(add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+;; ;; Document function parameters in echo area
+;; (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
 
 ;; This makes sure we're using company in all modes
 (require 'company)
